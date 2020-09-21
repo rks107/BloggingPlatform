86c5b1fc4d4fe7dcea0a8ec253308bdc2f7322d2
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-09-22 00:13:01 +0530

    home_controller and home page updated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,20 @@
-
+const conn = require("../config/mysql");
 
 
 
 
 
 module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+
+  conn.query(
+    `SELECT * FROM blog INNER JOIN user ON blog.user_id = user.id`,
+    function (err, results, fields) {
+
+      return res.render("home", {
+        title: "Home",
+        blogs: results,
+      });
+    }
+  );
+  
 };
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,9 @@ const conn = require("../config/mysql");
 
 // render Sign up page
 module.exports.signUp = function (req, res) {
-//   if (req.isAuthenticated()) {
-//     return res.redirect("/users/profile");
-//   }
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
 
   return res.render("user_sign_up", {
     title: "Sign Up",
@@ -14,9 +14,9 @@ module.exports.signUp = function (req, res) {
 // render Sign in page
 module.exports.signIn = function(req, res) {
 
-    // if(req.isAuthenticated()) {
-    //     return res.redirect('/users/profile');
-    // }
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
 
     return res.render('user_sign_in', {
         title : "Sign In"
@@ -59,7 +59,6 @@ module.exports.profile = function (req, res) {
 
 module.exports.destroySession = function (req, res) {
   req.logout();
-  // req.flash("success", "You have logged out");
   return res.redirect("/");
 };
 
